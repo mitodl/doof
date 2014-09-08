0047155c6d92298e7f6023db04e8d17b050944c5
Carson Gee <x@carsongee.com>
2014-09-08 09:16:50 -0400

    Normalizing gnome board

--- plugins/inator/reply.py
+++ plugins/inator/reply.py
@@ -8,6 +8,27 @@ from will.decorators import respond_to, hear
 
 class RepliesPlugin(WillPlugin):
 
+    def get_jid(self, nick_or_name):
+        jid = None
+        for jid, info in self.internal_roster.items():
+            if (info['nick'] == nick_or_name or
+                    nick_or_name in info['name'] or
+                    info['nick'] == nick_or_name.lstrip('@')):
+                jid = jid
+                break
+        return jid
+
+    def plus_one_gnome(self, nick):
+        user_id = self.get_jid(nick)
+
+        gnomes = self.load('garden_gnomes', {})
+        user_gnomes = int(gnomes.get(user_id, 0))
+        if not user_gnomes:
+            gnomes[user_id] = 1
+        else:
+            gnomes[user_id] = 1 + user_gnomes
+        self.save('garden_gnomes', gnomes)
+
     @respond_to("^any new schemes\?")
     def schemeinator(self, message):
         """scheme: any new schemes?"""
@@ -28,19 +49,25 @@ class RepliesPlugin(WillPlugin):
         garden_gnomes: award special recognition
         """
         gnomes = self.load("garden_gnomes", {})
-        user_gnomes = int(gnomes.get(user_name, 0))
+        # Look up user in roster
+        user_id = self.get_jid(user_name)
+        if not user_id:
+            self.reply('Unable to find {0}'.format(user_name))
+
+        user_gnomes = int(gnomes.get(user_id, 0))
         if not user_gnomes:
-            gnomes[user_name] = int(num_gnomes)
+            gnomes[user_id] = int(num_gnomes)
         else:
-            gnomes[user_name] = int(num_gnomes) + user_gnomes
+            gnomes[user_id] = int(num_gnomes) + user_gnomes
         self.save("garden_gnomes", gnomes)
+
         self.say("Awarded {0} gnomes to {1}.".format(
             num_gnomes,
             user_name
         ), message=message)
 
     @respond_to("garden gnome tally")
-    def gold_star_tally(self, message):
+    def garden_gnome_tally(self, message):
         """
         garden_gnomes: tally
         """
@@ -49,7 +76,13 @@ class RepliesPlugin(WillPlugin):
                                reverse=True)
         response = ['Garden gnomepocalypse leader board: <br /><ol>']
         for users in sorted_gnomes:
-            response.append('<li>{1} - {0}</li>'.format(*users))
+            user = self.get_user_by_jid(users[0])
+            if not user:
+                continue
+            response.append('<li>{0} - {1}</li>'.format(
+                users[1],
+                user['name']
+            ))
 
         self.reply(
             message,
@@ -59,22 +92,8 @@ class RepliesPlugin(WillPlugin):
 
     @hear("thanks?( you)?")
     def thank_you_gnome(self, message):
-        user = message.sender['nick']
-        gnomes = self.load('garden_gnomes', {})
-        user_gnomes = int(gnomes.get(user, 0))
-        if not user_gnomes:
-            gnomes[user] = 1
-        else:
-            gnomes[user] = 1 + user_gnomes
-        self.save('garden_gnomes', gnomes)
+        self.plus_one_gnome(message.sender['nick'])
 
     @hear("please")
     def please_gnome(self, message):
-        user = message.sender['nick']
-        gnomes = self.load('garden_gnomes', {})
-        user_gnomes = int(gnomes.get(user, 0))
-        if not user_gnomes:
-            gnomes[user] = 1
-        else:
-            gnomes[user] = 1 + user_gnomes
-        self.save('garden_gnomes', gnomes)
+        self.plus_one_gnome(message.sender['nick'])
