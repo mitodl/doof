2d0c2b340dc09da25b34a6642b475422703428f1
Carson Gee <x@carsongee.com>
2015-04-02 17:08:35 -0400

    Repo statistics

---
+++ plugins/git/stats.py
@@ -0,0 +1,59 @@
+"""
+Github repository related commands like listing repos in an org,
+finding a repo, or other nonsense against the repo API.
+"""
+
+import logging
+
+from will.plugin import WillPlugin
+from will.decorators import respond_to
+
+from plugins.git.base import GithubBaseMixIn
+
+log = logging.getLogger('doof')
+
+
+class GitHubStatsPlugin(WillPlugin, GithubBaseMixIn):
+
+    @respond_to(
+        'magic mirror in the cloud, who is the busiest repo of them all'
+    )
+    def most_popular_repo(self, message):
+        """
+        github: magic @doof on the cloud, who is the busiest repo of them all
+        """
+        self.reply(
+            message,
+            'Hold on, Perry the Judgeapuss, this is a LOT of API calls'
+        )
+        url = 'orgs/{0}/repos'
+        repos = []
+        for org in ['mitodl', 'mitocw']:
+            repo_results, _ = self.get_all(False, url.format(org))
+            repos.extend(repo_results)
+        repos_by_commits = []
+        for repo in repos:
+            stats, _ = self.get_all(
+                False,
+                'repos/{full_name}/stats/participation'.format(
+                    full_name=repo['full_name']
+                )
+            )
+            if stats and stats.get('all'):
+                all_commits = sum(stats['all'])
+                repos_by_commits.append((all_commits, repo['html_url']))
+        repos_by_commits.sort(key=lambda tuple: tuple[0])
+        repos_by_commits.reverse()
+        self.reply(
+            message,
+            '{url} is the busiest of them all!'.format(
+                url=repos_by_commits[0][1]
+            )
+        )
+        next_four = [x[1] for x in repos_by_commits[1:4]]
+        self.reply(
+            message,
+            'Followed shortly by:\n{runners_up}'.format(
+                runners_up='\n'.join(next_four)
+            )
+        )
