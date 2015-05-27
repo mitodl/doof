b90ce83acf602a1259f05c3cd87ab56619b7c537
Shawn Milochik <shawn@milochik.com>
2015-05-27 19:05:19 -0400

    adds some docstrings and a "hmmm" message

--- plugins/inator/hear.py
+++ plugins/inator/hear.py
@@ -1,3 +1,8 @@
+"""
+Listen to the chat and interject blather if certain keywords
+are mentioned.
+"""
+
 import random
 
 import requests
@@ -21,10 +26,12 @@ class InatorHear(WillPlugin):
 
     @hear('oo+h')
     def ohmy(self, message):
+        """impersonate a second-grader"""
         self.say('ooooooohhh', message=message)
 
     @hear('hmm+')
     def hmm(self, message):
+        """let's bring the conversation back around to me"""
         response_list = [
             'Hmm indeed, let us contemplate my childhood in Drusselstein',
             ('Hmmm, do you think my theme song is too short?  '
@@ -32,13 +39,14 @@ class InatorHear(WillPlugin):
             'Hmm, I was just thinking how much I hate my lousy brother',
             ('Hmm, I was just wondering too. About how awesome the '
              'gnomepocalypse was'),
+            "That's a deep thought. Please tell us more.",
             ]
         self.say(random.choice(response_list), message=message)
 
     # This is stolen from edX's alton bot: https://github.com/edx/alton/pull/22
     @hear("alot")
     def alot(self, message):
-        """show off a picture of an alot when someone is talking about one """
+        """show off a picture of an alot when someone is talking about one"""
         data = {"q": "alot", "v": "1.0", "safe": "active", "rsz": "8"}
         response = requests.get(
             "http://ajax.googleapis.com/ajax/services/search/images",
@@ -51,5 +59,6 @@ class InatorHear(WillPlugin):
 
     @hear('surely')
     def surely(self, message):
+        """70s jokes are back!"""
         self.say("Don't call me Shirley", message=message)
 
